47d6b568ac2e90b122bcc3fb65da8e818a00bd5c
phvalguima <pedro.guimaraes@canonical.com>
2025-03-24 14:37:11 +0100

    Update bin-wrapper.sh

--- scripts/wrappers/bin-wrapper.sh
+++ scripts/wrappers/bin-wrapper.sh
@@ -2,15 +2,10 @@
 
 set -e
 
+OPENSEARCH_JAVA_OPTS="-Xms1g -Xmx1g"
+
 "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid snap_daemon \
     --regid snap_daemon -- \
-    "${JAVA_HOME}"/bin/java \
-    -Xms256m -Xmx256m \
-    -Xshare:auto \
-    -Djava.security.manager=allow \
-    -cp "${OPENSEARCH_LIB}/*" \
-    -Dopensearch.path.home="${OPENSEARCH_HOME}" \
-    -Dopensearch.path.conf="${OPENSEARCH_PATH_CONF}" \
-    org.opensearch.bootstrap.OpenSearch "${@}"
+    ${OPENSEARCH_BIN}/${bin_script} "${@}"
