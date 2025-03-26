4b0096f9abb6e0da68128df5c9a2e69a1547e256
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2025-03-26 11:17:44 +0100

    Add file for extra settings

--- scripts/wrappers/bin-wrapper.sh
+++ scripts/wrappers/bin-wrapper.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-OPENSEARCH_JAVA_OPTS="-Xms1g -Xmx1g"
+if ! ls ${SNAP_DATA_CURRENT}/etc/java-extra-opts > /dev/null 2>&1; then
+    echo 'OPENSEARCH_JAVA_OPTS="-Xms1g -Xmx1g"' > ${SNAP_DATA_CURRENT}/etc/java-extra-opts
+fi
+
+source "${SNAP_DATA_CURRENT}"/etc/java-extra-opts
 
 "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
