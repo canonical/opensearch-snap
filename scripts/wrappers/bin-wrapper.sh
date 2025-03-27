d92ff07293b95b9e2ad59ab9593990da26617c4d
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2025-03-27 17:50:29 +0100

    Add default value to java opts

--- scripts/wrappers/bin-wrapper.sh
+++ scripts/wrappers/bin-wrapper.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+if [ -z "${OPENSEARCH_JAVA_OPTS}" ]; then
+    OPENSEARCH_JAVA_OPTS="-Xms1g -Xmx1g"
+fi
+
 OPENSEARCH_JAVA_OPTS="${OPENSEARCH_JAVA_OPTS}" "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid snap_daemon \
