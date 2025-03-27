128f1ba4e8b27d47589493b32078c0766842418b
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2025-03-27 17:42:37 +0100

    Remove the need for file && export

--- scripts/wrappers/bin-wrapper.sh
+++ scripts/wrappers/bin-wrapper.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-if ! ls ${SNAP_DATA_CURRENT}/etc/opensearch/java-extra-opts > /dev/null 2>&1; then
-    echo 'export OPENSEARCH_JAVA_OPTS="-Xms1g -Xmx1g"' | tee ${SNAP_DATA_CURRENT}/etc/opensearch/java-extra-opts
-fi
-
-source "${SNAP_DATA_CURRENT}"/etc/opensearch/java-extra-opts
-
-"${SNAP}"/usr/bin/setpriv \
+OPENSEARCH_JAVA_OPTS="${OPENSEARCH_JAVA_OPTS}" "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid snap_daemon \
     --regid snap_daemon -- \
