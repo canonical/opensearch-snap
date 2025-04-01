4a669e6a87786f151da30302fec3dc2aab0924cf
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2025-04-01 18:29:31 +0200

    Add QAT support

--- scripts/wrappers/bin-wrapper.sh
+++ scripts/wrappers/bin-wrapper.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-"${SNAP}"/usr/bin/setpriv \
+if [ -z "${OPENSEARCH_JAVA_OPTS}" ]; then
+    OPENSEARCH_JAVA_OPTS="-Xms1g -Xmx1g"
+fi
+
+OPENSEARCH_JAVA_OPTS="${OPENSEARCH_JAVA_OPTS}" "${SNAP}"/usr/bin/setpriv \
     --clear-groups \
     --reuid snap_daemon \
     --regid snap_daemon -- \
